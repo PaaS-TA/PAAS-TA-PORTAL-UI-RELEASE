e712e821402463aefa35ae4509944a14156d3d3e
swmoon <swmoon@bluedigm.com>
2019-09-17 13:47:46 +0900

    ubuntu-xenial 지원되도록 변경 ubuntu-trusty 미지원

--- delete.sh
+++ delete.sh
@@ -1,5 +1,5 @@
 bosh delete-deployment -d paasta-portal-ui --force
 rm -rf dev_releases/
 
-rm -rf paas-ta-portal-ui-release-1.0.tgz
-bosh delete-release paasta-portal-ui-release/1.0 --force
+rm -rf paas-ta-portal-ui-release-2.0.tgz
+bosh delete-release paasta-portal-ui-release/2.0 --force
